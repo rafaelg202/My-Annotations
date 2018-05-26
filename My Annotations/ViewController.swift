61402645e94cba532cea42b02f8b61f445b2ac5f
Rafael <rafaelg202@gmail.com>
2018-05-26 16:42:39 -0300

    App Finished

--- My Annotations/ViewController.swift
+++ My Annotations/ViewController.swift
@@ -10,10 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textoCampo: UITextView!
+    let chave = "minhaAnotacao"
+    @IBAction func salvarAnotacao(_ sender: Any) {
+        
+        if let texto = textoCampo.text{
+            
+            UserDefaults.standard.set(texto, forKey: chave)
+            
+        }
+        
+    }
+    
+    func recuperaAnotacao() -> String {
+        
+        if let textoRecuperado = UserDefaults.standard.object(forKey: chave){
+            return textoRecuperado as! String
+            //return String(textoRecuperado)
+            
+        }
+        
+        return ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        UserDefaults.standard.set("azul", forKey: "corFundo")
+        textoCampo.text = recuperaAnotacao()
         
     }
 
